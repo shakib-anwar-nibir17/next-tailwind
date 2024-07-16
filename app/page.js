1bb9748eac1554ef19d9f2f43573a13705779656
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-07-16 18:51:39 +0600

    first commit

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-[600px] h-screen bg-red-600 text-white p-40">
+      <div className="w-full h-full bg-blue-500"></div>
+    </div>
+  );
+}
